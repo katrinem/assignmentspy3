09f77f59731927d7c1753c5df420acc6eed83929
Katrin Edda <katrin.ebda@gmail.com>
2020-09-03 14:28:26 +0000

    Added comments that describe the algorithm for max_int.py

---
+++ max_int.py
@@ -0,0 +1,9 @@
+# Program that finds the maximum positive integer input by a user. 
+# The user repeatedly inputs numbers until a negative value is entered.
+
+# Get two inputs from user to start the loop
+
+# While the user enters positive values, compare the two values
+# and keep the larger one. Then ask for a new one
+
+# As soon as the input is negative, exit the loop and print the maximum
