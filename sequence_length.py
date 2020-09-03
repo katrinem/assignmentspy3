4c4cd91c0598f72c071b1573829997b42825f7f0
Katrin Edda <katrin.ebda@gmail.com>
2020-09-03 15:03:34 +0000

    Write comments that describe the algorithm for sequence_length.py

---
+++ sequence_length.py
@@ -0,0 +1,9 @@
+# Program that generates the first n numbers in the sequence:; 1, 2, 3, 6, 11, 20, 37, ...
+# Each number is the sum of the three numbers before.
+
+n = int(input("Enter the length of the sequence: "))
+
+# Initialize the values that will be summed up in the loop, and a counter
+
+# Run a loop that prints n sums of three integers, updating the integers
+# according to the sequence each time
