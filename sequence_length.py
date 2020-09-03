7b515dd26c21cc30e2f63d1896adfac8158a9a5f
Katrin Edda <katrin.ebda@gmail.com>
2020-09-03 15:22:18 +0000

    Write and test the program for sequence_length.py

--- sequence_length.py
+++ sequence_length.py
@@ -3,7 +3,22 @@
 
 n = int(input("Enter the length of the sequence: "))
 
-# Initialize the values that will be summed up in the loop, and a counter
+# Initialize the values that will be summed up in the loop
+first_int = 1
+second_int = 2
+third_int = 3
+
+# Print the first three numbers in the sequence
+print(first_int)
+print(second_int)
+print(third_int)
 
 # Run a loop that prints n sums of three integers, updating the integers
 # according to the sequence each time
+for i in range(2,n):
+    next_int = first_int + second_int + third_int
+    print(next_int)
+    first_int = second_int
+    second_int = third_int
+    third_int = next_int
+    
